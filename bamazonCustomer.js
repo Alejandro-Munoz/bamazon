b5606feecefa003e94224e02c7b124b0eb06b3be
Christian Escobar <crs.escobar@gmail.com>
2018-02-05 13:00:05 -0800

    display items in table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql2/promise");
+const table = require("cli-table");
+const inquirer  = require("inquirer";)
 const connectionDetails = require("./connectionDetails");
 
 function displayAllItems() {
@@ -14,12 +16,36 @@ function displayAllItems() {
 			return results;
 		})
 		.then((results) => {
-			console.log(results);
+			displayData(results);
 		}, function(error) {
 			if(error) {
 				throw error
 			}
 		});
+}
+
+function displayData(results) {
+
+	let dataTable = new table({
+		head: ["Item Id", "Product Name", "Department Name", "Price", "Stock Qty"],
+		colWidths: [10, 20, 20, 10, 15]
+	});
+
+	for(let i=0; i<results[0].length; i++) {
+		const row = [results[0][i].item_id, 
+			results[0][i].product_name, 
+			results[0][i].department_name, 
+			parseFloat(results[0][i].price).toFixed(2), 
+			results[0][i].stock_quantity];
+
+		dataTable.push(row);
+	}
+
+	console.log(dataTable.toString());
+}
+
+function promptUserToBuy() {
+
 }
 
 module.exports = displayAllItems;
