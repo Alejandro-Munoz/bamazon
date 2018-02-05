0e6596700d96bd59a09e138598f4f9e1896b4fc6
Christian Escobar <crs.escobar@gmail.com>
2018-02-05 15:25:15 -0800

    prompt user for order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 const table = require("cli-table");
-const inquirer  = require("inquirer";)
+const inquirer  = require("inquirer");
 const connectionDetails = require("./connectionDetails");
 
 function displayAllItems() {
@@ -17,9 +17,11 @@ function displayAllItems() {
 		})
 		.then((results) => {
 			displayData(results);
-		}, function(error) {
+
+			promptUserToBuy(results);
+		}).catch(function(error) {
 			if(error) {
-				throw error
+				throw error;
 			}
 		});
 }
@@ -44,8 +46,47 @@ function displayData(results) {
 	console.log(dataTable.toString());
 }
 
-function promptUserToBuy() {
+function promptUserToBuy(results) {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "itemID",
+				message: "Please enter the ID of the item you would like to buy:",
+				validate: function(input) {
+					if(isNaN(input) || !doesItemIdExist(results, input)) {
+						return "That item ID does not exist, please choose a valid ID.";
+					} 
 
+					return true;
+				}
+			},
+			{
+				type: "input",
+				name: "quantity",
+				message: "How many of that item would you like to purchase?",
+				validate: function(input) {
+					if(isNaN(input)) {
+						return "That is an invalid amount.  Please enter a valid number.";
+					}
+
+					return true;
+				}
+			}
+		]).then((answers) => {
+			console.log("OK cool!");
+		});
+}
+
+function doesItemIdExist(results, itemID) {
+	for(let i=0; i<results[0].length; i++) {
+		if(results[0][i].item_id === parseInt(itemID)) {
+			
+			return true;
+		}
+	}
+
+	return false;
 }
 
 module.exports = displayAllItems;
