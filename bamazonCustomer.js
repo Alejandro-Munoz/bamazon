ea4dee5e7819991345ec79ca4eba73077101c90e
Christian Escobar <crs.escobar@gmail.com>
2018-02-07 11:17:24 -0800

    work in progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ const mysql = require("mysql2/promise");
 const table = require("cli-table");
 const inquirer  = require("inquirer");
 const connectionDetails = require("./connectionDetails");
+const databaseQueries = require("./databaseQueries");
+const bamazonUtils = require("./bamazonUtils");
 
 /*
 	Starting point
@@ -14,19 +16,9 @@ function bamazonCustomer() {
 	Queries the products table and then will prompt the user whether they would like to make a purchase
 */
 function displayAllItems() {
-	const sqlQuery = "SELECT * FROM products ORDER BY item_id";
-
-	mysql.createConnection(connectionDetails())
-		.then((conn) => {
-			// Execute query
-			const results = conn.query(sqlQuery);
-
-			conn.end();
-
-			return results;
-		})
+	databaseQueries.getAllProducts()
 		.then((results) => {
-			displayData(results);
+			bamazonUtils.displayData(results);
 
 			promptUserToBuy(results);
 		}).catch(function(error) {
@@ -34,29 +26,6 @@ function displayAllItems() {
 				console.error(error);
 			}
 		});
-
-		/*
-			Loops through the product table rows and creates a table for display
-		*/
-		function displayData(results) {
-
-			let dataTable = new table({
-				head: ["Item Id", "Product Name", "Department Name", "Price", "Stock Qty"],
-				colWidths: [10, 20, 20, 10, 15]
-			});
-
-			for(let i=0; i<results[0].length; i++) {
-				const row = [results[0][i].item_id, 
-					results[0][i].product_name, 
-					results[0][i].department_name, 
-					parseFloat(results[0][i].price).toFixed(2), 
-					results[0][i].stock_quantity];
-
-				dataTable.push(row);
-			}
-
-			console.log(dataTable.toString());
-		} // End displayData
 
 		/*
 			Prompts user to provide an item ID for purchase, then will prompt for the quantity.
--- bamazonManager.js
+++ bamazonManager.js
@@ -42,28 +42,6 @@ function promptUser() {
 				});
 		} // End displayAllProducts
 
-		/*
-		function displayData(results) {
-
-			let dataTable = new table({
-				head: ["Item Id", "Product Name", "Department Name", "Price", "Stock Qty"],
-				colWidths: [10, 20, 20, 10, 15]
-			});
-
-			for(let i=0; i<results[0].length; i++) {
-				const row = [results[0][i].item_id, 
-					results[0][i].product_name, 
-					results[0][i].department_name, 
-					parseFloat(results[0][i].price).toFixed(2), 
-					results[0][i].stock_quantity];
-
-				dataTable.push(row);
-			}
-
-			console.log(dataTable.toString());
-		} // End displayData
-		*/
-
 		function displayLowInventory() {
 			const sqlQuery = "SELECT * FROM products " +
 										" WHERE stock_quantity < 5";
